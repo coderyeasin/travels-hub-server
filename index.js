575ce9096f6787a22c296c405f54a19144395d83
coderyeasin <yeasin3233@gmail.com>
2021-10-31 10:12:42 +0600

    more api

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,15 +27,50 @@ async function server() {
 
         const database = client.db('travels_info')
         const tourplanCollection = database.collection('tourism')
+        const touristCollection = client.db('tourist').collection('users')
 
-        console.log('database is connected');
-
-        //GET API
+        // console.log('database is connected');
+            //POST API -users
+            app.post('/tourism', async (req, res) => {
+                const newUser = req.body;
+                const result = await tourplanCollection.insertOne(newUser)
+                res.send(result)
+            })
+         //GET API
         app.get('/tourism', async (req, res) => {
             console.log('dekho body te', req.body);
             // const tourInfo = req.body;
             const result = await tourplanCollection.find({}).toArray()
             res.send(result)
+        })
+
+    
+       //POST API -users
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await touristCollection.insertOne(newUser)
+            res.send(result)
+        })
+
+        //GET API -users 
+        app.get('/users', async (req, res) => {
+            console.log('you can', req.body);
+            // const tourInfo = req.body;
+            const result = await touristCollection.find({}).toArray()
+            res.send(result)
+        })
+        //myZone
+        app.get('/myzones/:email', async (req, res) => {
+            const result = await touristCollection.find({email: req.params.email}).toArray()
+            res.send(result)
+        })
+
+        //delete
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await touristCollection.deleteOne(query)
+            res.json(result)
         })
     }
     finally {
@@ -48,5 +85,4 @@ server().catch(console.dir)
 app.listen(port, () => {
     console.log(`guys listening port is: ${port}`);
 })
-
 
